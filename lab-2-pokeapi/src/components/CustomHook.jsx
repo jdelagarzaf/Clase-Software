96ced4e29c165b71a7d305abe81c4a09d10fc511
Jorge Adrian de la Garza Flores <52385984+jdelagarzaf@users.noreply.github.com>
2026-02-19 09:28:51 -0600

    change to disney api

--- lab-2-pokeapi/src/components/CustomHook.jsx
+++ lab-2-pokeapi/src/components/CustomHook.jsx
@@ -5,23 +5,22 @@ import { useCounter } from '../hooks/useCounter'
 import { useFetch } from '../hooks/useFetch'
 
 export const CustomHook = () => {
-    const { counter, increment, decrement, reset } = useCounter(1)
-    const { data, isLoading, hasError } = useFetch(`https://pokeapi.co/api/v2/pokemon/${counter}`)
+    const { counter, increment, decrement, reset } = useCounter(350)
+    const { data, isLoading, hasError } = useFetch(`https://api.disneyapi.dev/character/${counter}`)
   return (
     <>
-        <h1>Información de API de Pokemon</h1>
+        <h1>Información de API de Disney</h1>
         <hr/>
-        <h2>{data?.name}</h2>
-        {isLoading ? <Loading/> : <Card id={counter} name={data.name} sprites={[
-            data.sprites?.front_default,
-            data.sprites?.back_default,
-            data.sprites?.front_shiny,
-            data.sprites?.back_shiny
-        ]}/>}
-
         <button className='btn btn-danger' onClick={() => decrement()}>Anterior</button>
         <button className='btn btn-primary' onClick={() => increment()}>Siguiente</button>
         <button className='btn btn-info' onClick={() => reset()}>Reset</button>
+        
+        <h2>{data?.data?.name}</h2>
+        {isLoading ? <Loading/> : <Card id={counter} name={data.data.name} sprites={[
+            data.data.imageUrl
+        ]}/>}
+
+        
     </>
   )
 }
