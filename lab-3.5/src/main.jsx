04007dc3a0dbf6b184b1c27dc92ccf7423900bdc
Jorge Adrian de la Garza Flores <52385984+jdelagarzaf@users.noreply.github.com>
2026-03-02 07:54:44 -0600

    add lab 4 crud

--- lab-3.5/src/main.jsx
+++ lab-3.5/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import AppUsuarios from './Usuarios.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppUsuarios />
+    <App />
   </StrictMode>,
 )
